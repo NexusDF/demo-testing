a9e8586fe3b5b8657dc2ad447ccdd43d4c4081cb
NexusDF <zfassaiz@yandex.ru>
2025-09-22 19:45:46 +0400

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h1 class="heading">Demo testing Github page!</h1>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  min-height: 100dvh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.heading {
+  font-size: 32px;
+  color: #333;
+}
+</style>
